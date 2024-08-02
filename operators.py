2c7e6be60f5447dfb1a2227a396240dcd87390fd
zakir <azizizakir449@gmail.com>
2024-08-02 22:29:15 +0500

    my commit

---
+++ operators.py
@@ -0,0 +1,18 @@
+# Arithmetic Operators
+a = 7
+b = 4 
+c = a + b
+print(c)
+
+# Assignment Operaters
+a = 4-2 #Assignment 4-2 in a
+print(a)
+b = 6
+b += 3 # Increment the value of b by 3 and then assign it to b
+print(b)
+
+# Comparison Operator
+ 
+d = 5==6
+
+print(d)
--- rules_variable.py
+++ rules_variable.py
@@ -8,5 +8,5 @@ sameera = 45
 
 _zakir = 34
 
-# @swamera = 56 Invalid
-# s_meera #
+# @swamera = 56 # Invalid due to @  symbol
+# s_meera # Invalid due to @  symbol
