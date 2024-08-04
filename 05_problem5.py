0016ad7b699a767ab815743dfc32c77383d4878f
zakir <azizizakir449@gmail.com>
2024-08-04 17:56:26 +0500

    my commit

---
+++ 05_problem5.py
@@ -0,0 +1,4 @@
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+print("the average of these two number is ", (a+b)/2)
---
+++ 06_problem6.py
@@ -0,0 +1,7 @@
+a = int(input("Enter your number: "))
+
+
+print("the square of the number is", a**2)
+print("the square of the number is", a*a)
+# print("the square of the number is", a^a) # Incorrect forfinding square of anumber 
+# python
